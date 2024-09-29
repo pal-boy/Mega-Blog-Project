a8bb0832e1fe0032d6f5ecb0d40eef12c79b53d1
Aakash Pal <akashpal1482@gmail.com>
2024-09-29 21:24:20 +0530

    adding RTE in progress

---
+++ src/components/RTE.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Editor} from '@tinymce/tinymce-react'
+
+export default function RTE() {
+  return (
+    <Editor
+    initialValue='default value'
+    init={
+        {branding:false,
+            height:500,
+            menubar:true,
+            plugins: [
+                "image","advlist","autolink","lists","link","image","charmap","preview","anchor","searchreplace","visualblocks","code","fullscreen","insertdatetime","media","table","code","help","wordcount","anchor",
+            ],
+            toolbar:
+            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        }
+    }
+    />
+  )
+}
+
+

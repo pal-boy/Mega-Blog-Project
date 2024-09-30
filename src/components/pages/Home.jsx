f66543d25eaf78d770fed03858cccd6a0c3950f7
Aakash Pal <akashpal1482@gmail.com>
2024-09-30 10:08:03 +0530

    first debugging done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
-import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
+import  {React,useEffect, useState} from 'react'
+import appwriteService from "../../appwrite/config";
+import {Container, PostCard} from '../index'
 
 function Home() {
     const [posts, setPosts] = useState([])
